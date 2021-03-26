331230aba48088cef97bb59789e6143c31f04baa
Justyna Uryniak <bucka.justyna@interia.pl>
2021-03-26 23:00:42 +0100

    Zmienne

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+    String welcome = "Hello, my name is Justyna";
+        System.out.println(welcome);
+    String weekday = "Friday";
+        System.out.println(weekday);
+    int temperature = 9;
+        System.out.println(temperature);
+    double time = 21.42;
+        System.out.println(time);
+    char gender = 'K';
+        System.out.println(gender);
+    boolean isRemoteCourse = true;
+        System.out.println(isRemoteCourse);
+    }
+}
