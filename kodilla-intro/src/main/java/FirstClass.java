ccf169cb00f6db41dbbe9053f4f7f4f94f7b0cd4
Justyna Uryniak <bucka.justyna@interia.pl>
2021-03-30 20:50:44 +0200

    Leap Year

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String welcome = ("Hello from FirstClass!");// [2]
-        System.out.println(welcome); // [3]
+        System.out.println("Hello from FirstClass!");
+
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
     }
 }
---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,12 @@
+public class LeapYear {
+    public static void main(String[] args) {
+       int year = 2004;
+
+            if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0) {
+
+                System.out.println("The year is leap");
+            } else {
+                System.out.println("The year isn't leap");
+            }
+        }
+    }
