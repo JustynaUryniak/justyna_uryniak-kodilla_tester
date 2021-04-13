011ac8e8bdd66572d7ae498f99e3b097ddde043f
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-13 23:58:10 +0200

    Odczyt atrybutów

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,7 +1,7 @@
 public class LeapYear {
     public static void main(String[] args) {
         int year = 2000;
-        boolean isLeapYear;
+        boolean isLeapYear = true;
 
         if (isLeapYear) {
             System.out.println("The year is leap");
