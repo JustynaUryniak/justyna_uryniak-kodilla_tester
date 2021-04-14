aba9f4922418481bfbed24fcbb633ee1b3b477c8
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-14 18:53:55 +0200

    tablica obiektow

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,26 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public int lastGrades() {
+        return this.grades[this.size-1];
+    }
+    public double averageGrades() {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum = this.grades[i];
+        }
+        return sum / this.size;
+    }
+}
