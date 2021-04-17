2a10070f835b3ef4c02f4c472884a38e529cc7bc
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-17 23:21:48 +0200

    zmienne i metody statyczne

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,15 +1,14 @@
 public class Book {
-    private String author;
-    private String title;
-
-            public static Book of(String author, String title) {
-                Book.author = author;
-                Book.title = title;
-            }
     public static void main(String[] args) {
-        Book book = new Book();
-        Book book = Book.of("Isaac Asimov", "The Galaxy");
-        System.out.println(book);
+        private String author;
+        private String title;
     }
-}
+            public static Book of(String author, String title) {
+            Book book = new Book();
+            Book book = Book.of("Isaac Asimov", "The Galaxy");
+                this.author = author;
+                this.title = title;
+            System.out.println(book);
+            }
+    }
 
