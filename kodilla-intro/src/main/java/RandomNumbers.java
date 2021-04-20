15d675113557694622c3547d4aeacd05139fafbc
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-20 18:55:29 +0200

    Pętle

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,34 +1,39 @@
 import java.util.Random;
 
 public class RandomNumbers {
-    public static void main(String[] args) {
+    int result = 0;
+    int sum = 0;
+    int min = 30;
+    int max = 0;
+
+    public int getCountOfRandomNumber() {
         Random random = new Random();
-        int result = 0;
-        int sum = 0;
-        int min = 0;
-        int max = 0;
         while (sum < 5000) {
             int temp = random.nextInt(31);
             sum = sum + temp;
             result++;
         }
         return result;
+    }
 
-        public int getMax() {
-            int result = 0;
-            int max = 30;
-            for (int i = 0; i > max; i++) {
-                max = result;
+    public int getMax(int temp, int max) {
+            int max = 0;
+        for (int i = 0; i < temp; i++) {
+            if (temp > max) {
+                max = temp;
             }
-            return = max;
         }
-        public int getMin() {
-            int result = 0;
-            int min = 0;
-            for (int i = 0; i < min; i++) {
-                min = result;
+        return max;
+    }
+
+    public int getMin(int temp, int min) {
+            int min = 30;
+        for (int i = 0; i < temp; i++) {
+            if (temp < min) {
+                min = temp;
             }
-            return = min;
         }
+        return min;
     }
 }
+
