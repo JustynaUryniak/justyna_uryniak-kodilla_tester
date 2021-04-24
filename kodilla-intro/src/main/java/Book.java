fb4162f534408abdb0efac45f901e8cb9d204b38
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-24 22:39:27 +0200

    Book

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,15 +1,19 @@
 public class Book {
-    private String author;
-    private String title;
+     String author;
+     String title;
 
     public static Book of(String author, String title) {
         Book book = new Book();
+        book.author = author;
+        book.title = title;
+        return book;
+    }
+    public static void main(String[] args) {
         Book book = Book.of("Isaac Asimov", "The Galaxy");
-        book.author = "Isaac Asimov";
-        book.title = "The Galaxy";
-
-        System.out.println("book.author", "book.title");
+        System.out.println(book.author);
+        System.out.println(book.title);
     }
 }
+
 
 
