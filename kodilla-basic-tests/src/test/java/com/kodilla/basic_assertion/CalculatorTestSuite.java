f69c624f8e4ae10e18c863ac122b6010336a7685
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-24 20:40:04 +0200

    testy jednostkowe dla kalkulatora

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
+
     @Test
     public void testSum() {
         Calculator calculator = new Calculator();
