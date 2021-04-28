ea0a39531770b4a740574f922ebd7d917a940a66
Justyna Uryniak <bucka.justyna@interia.pl>
2021-04-28 22:18:32 +0200

    kolekcje

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -0,0 +1,26 @@
+package com.kodilla.collections.arrays.homework;
+
+
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Mercedes;
+
+public class CarUtils {
+    public static void describeCar(Car car) {
+        System.out.println("-----------------------------");
+        System.out.println("The car brand: " + getCarBrand(car));
+        System.out.println("Car speed: " + car.getSpeed());
+        car.increaseSpeed();
+    }
+    private static String getCarBrand(Car car) {
+        if (car instanceof Ford)
+            return "Ford";
+        else if (car instanceof Kia)
+            return "Kia";
+        else if (car instanceof Mercedes)
+            return "Mercedes";
+        else
+            return "Unknown car brand";
+    }
+}
---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -0,0 +1,32 @@
+package com.kodilla.collections.arrays.homework;
+
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Kia;
+import com.kodilla.collections.interfaces.homework.Mercedes;
+
+import java.util.Random;
+
+public class CarsApplication {
+    public static void main(String[] args) {
+        Car[] cars = new Car[15];
+        for (int n = 0; n <cars.length; n++)
+            cars[n] = drawCar();
+        for (Car car: cars)
+            CarUtils.describeCar(car);
+    }
+
+    public static Car drawCar() {
+        Random random = new Random();
+        int drawnCarBrand = random.nextInt(3);
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+        int c = random.nextInt(100);
+        if (drawnCarBrand == 0)
+            return new Ford(a);
+        else if (drawnCarBrand == 1)
+            return new Kia(b);
+        else
+            return new Mercedes(c);
+    }
+}
